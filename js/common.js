d017a92b3fe86866db4d997bd7389e02dfca017c
mrrin <nitroute@gmail.com>
2020-10-28 14:06:39 +0900

    first

---
+++ js/common.js
@@ -0,0 +1,16 @@
+
+//ヘッダーの高さ分だけコンテンツを下げる
+$(function() {
+  var height=$("header").height();
+  $("head-title").css("margin-top", height + 10);//10pxだけ余裕をもたせる
+});
+
+//ハンバーガーメニュー
+
+(function($) {
+    $(function () {
+      $('#nav-toggle').on('click', function() {
+        $('body').toggleClass('open');
+      });
+    });
+})(jQuery);
